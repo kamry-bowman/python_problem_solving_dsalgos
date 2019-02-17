e0d65c8bf097e7a274e7cbeb50dc1255587f5993
Kamry Bowman <kamry.bowman@gmail.com>
2019-02-17 09:59:17 -0700

    implement infix evaluator

--- chapter_3/operators.py
+++ chapter_3/operators.py
@@ -40,3 +40,72 @@ def infixToPostfix(infixexpr):
 print(infixToPostfix("A * B + C * D"))
 print(infixToPostfix("( A + B ) * C - ( D - E ) * ( F + G )"))
 print(infixToPostfix('5 * 3 ** ( 4 - 2 )'))
+
+
+def doMath(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    else:
+        return op1 - op2
+
+
+def infixEvaluator(exp):
+    prec = {}
+    prec["**"] = 4
+    prec["*"] = 3
+    prec["/"] = 3
+    prec["+"] = 2
+    prec["-"] = 2
+    prec["("] = 1
+    operatorStack = Stack()
+    operandStack = Stack()
+    tokenList = exp.split()
+
+    for token in tokenList:
+        if token == '(':
+            operatorStack.push(token)
+        elif token == ')':
+            topToken = operatorStack.pop()
+            while topToken != '(':
+                arg2 = operandStack.pop()
+                arg1 = operandStack.pop()
+                result = doMath(topToken, arg1, arg2)
+                operandStack.push(result)
+                topToken = operatorStack.pop()
+        else:
+            try:
+                precedence = prec[token]
+            except KeyError:
+                operandStack.push(float(token))
+            else:
+                while (not operatorStack.isEmpty()) and \
+                        (prec[operatorStack.peek()] >= precedence):
+                    operator = operatorStack.pop()
+                    arg2 = operandStack.pop()
+                    arg1 = operandStack.pop()
+                    result = doMath(operator, arg1, arg2)
+                    operandStack.push(result)
+                operatorStack.push(token)
+
+    print(operatorStack, operandStack)
+    while not operatorStack.isEmpty():
+        operator = operatorStack.pop()
+        arg2 = operandStack.pop()
+        arg1 = operandStack.pop()
+        result = doMath(operator, arg1, arg2)
+        operandStack.push(result)
+    if(operandStack.size() != 1):
+        print('error')
+    else:
+        return operandStack.pop()
+
+
+print(infixEvaluator('2 + 2'))
+print(infixEvaluator('2 + 2 * 3'))
+print(infixEvaluator('( 2 + 2 ) * 3'))
+print(infixEvaluator('2 * ( 2 + 2 ) * 3 + 1'))
+print(infixEvaluator('1 + ( 2 * ( 2 + 2 ) * 3 + 1 )'))
---
+++ chapter_3/ordered_list.py
@@ -0,0 +1,99 @@
+
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, newData):
+        self.data = newData
+
+    def setNext(self, newnext):
+        self.next = newnext
+
+
+class OrderedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            count += 1
+            current = current.getNext()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        stop = False
+        while current is not None and not found and not stop:
+            if current.getData() == item:
+                found = True
+            else:
+                if current.getData() > item:
+                    stop = True
+                else:
+                    current = current.getNext()
+
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while current != None and found == False:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+            return current.getData()
+
+    def add(self, item):
+        current = self.head
+        previous = None
+        stop = False
+        while current is not None and not stop:
+            if current.getData() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+        temp = Node(item)
+        if previous == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            previous.setNext(temp)
+            temp.setNext(current)
+
+
+mylist = OrderedList()
+
+mylist.add(31)
+mylist.add(77)
+mylist.add(17)
+mylist.add(93)
+mylist.add(26)
+mylist.add(54)
+
+print(mylist.size())
+print(mylist.search(3))
+print(mylist.remove(17))
+print(mylist.size())
+mylist.append(22)
+print(mylist.size())
